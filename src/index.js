2b0e2df79057b5491d39ad0e16465e9358300c85
Raul26_tech <rauletec2020@gmail.com>
2023-05-15 19:25:08 -0300

    Finalizando projeto

--- src/index.js
+++ src/index.js
@@ -24,6 +24,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+
+    return balance;
+  }, 0);
+}
+
 //CRIANDO CONTA DO USUÁRIO
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -51,6 +63,88 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
 
   return response.json(customer.statement);
+});
+
+//CRIANDO DEPOSÍTO
+app.post("/deposit", verifyIfExistsAccountCPF, (resquest, response) => {
+  const { description, amount } = resquest.body;
+
+  const { customer } = resquest;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+//CRIANDO SAQUE
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Fundos insulfiientes!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+// CAPTURANDO HORÁRIO DO DEPOSÍTO
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
+// ALTERANDO DADOS DO CLIENTE
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+//PEGANDO DADOS DO USUÁRIO
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+//DELETANDO DADOS DO USUÁRIO
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
 });
 
 app.listen(port);
